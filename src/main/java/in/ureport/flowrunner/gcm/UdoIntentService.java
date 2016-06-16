39b393323f3ecbb401f19be32cb74f4367041707
Felipe Gualberto <felipe.gualberto@ilhasoft.com.br>
2016-06-16 12:10:41 -0300

    Get Icon of Application

--- src/main/java/in/ureport/flowrunner/gcm/UdoIntentService.java
+++ src/main/java/in/ureport/flowrunner/gcm/UdoIntentService.java
@@ -4,7 +4,10 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -28,13 +31,13 @@ public class UdoIntentService extends GcmListenerService {
         String type = data.getString("type");
         String packageName = getApplicationContext().getPackageName();
         Intent launchIntent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(packageName);
-
-        PendingIntent pContentIntent = PendingIntent.getActivity(getApplicationContext(), 873459238,launchIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pContentIntent = PendingIntent.getActivity(getApplicationContext(), 873459238, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         if (type.equals("Rapidpro")) {
+            ApplicationInfo info = getApplicationInfo();
             Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
             NotificationCompat.Builder mBuilder =
                     new NotificationCompat.Builder(getApplicationContext())
-                            .setSmallIcon(R.drawable.img_button_play)
+                            .setSmallIcon(getApplicationInfo().icon)
                             .setContentTitle(message)
                             .setContentText(getApplicationContext().getText(R.string.new_flow))
                             .setSound(alarmSound)
@@ -43,7 +46,21 @@ public class UdoIntentService extends GcmListenerService {
             NotificationManager mNotificationManager =
                     (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
             mNotificationManager.notify(81723469, mBuilder.build());
-
         }
     }
 }
+//
+//    protected int getSmallIconId(Context context, Intent intent) {
+//        ApplicationInfo info = getApplicationInfo();
+//        info.i
+//        Bundle metaData = info.metaData;
+//        if (metaData != null) {
+//            String appPackageName=...; //use getPackageName() in case you wish to use yours
+//            final PackageManager pm=getPackageManager();
+//            final ApplicationInfo applicationInfo=pm.getApplicationInfo(packageName,PackageManager.GET_META_DATA);
+//            final Resources resources=packageManager.getResourcesForApplication(applicationInfo);
+//
+//        }
+//        return 1;
+//    }
+//}
