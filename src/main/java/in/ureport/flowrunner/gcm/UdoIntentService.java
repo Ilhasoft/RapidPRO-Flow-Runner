606fcb71f37fd0c7aa87b0488576cbb50a92c113
Felipe Gualberto <felipe.gualberto@ilhasoft.com.br>
2016-06-20 13:43:31 -0300

    Refactored Code

--- src/main/java/in/ureport/flowrunner/gcm/UdoIntentService.java
+++ src/main/java/in/ureport/flowrunner/gcm/UdoIntentService.java
@@ -5,47 +5,34 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
-
-import in.ureport.flowrunner.R;
 
 /**
  * Created by redmine on 5/30/16.
  */
 public class UdoIntentService extends GcmListenerService {
     private static final String TAG = "UdoIntentService";
+    public static String NotificationUDO = "notification_udo";
+    private String title;
+    private String type;
+    private String message;
+    private String packageName;
+    private Uri alarmSound;
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
-        String title = data.getString("gcm.notification.title");
-        String message = data.getString("message");
-        String type = data.getString("type");
-        String packageName = getApplicationContext().getPackageName();
-        Intent launchIntent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(packageName)
-                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pContentIntent = PendingIntent.getActivity(getApplicationContext(), 873459238, launchIntent, PendingIntent.FLAG_ONE_SHOT);
+        title = data.getString("gcm.notification.title");
+        message = data.getString("message");
+        type = data.getString("type");
+        packageName = getApplicationContext().getPackageName();
+        alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         if (type.equals("Rapidpro")) {
-            ApplicationInfo info = getApplicationInfo();
-            Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            NotificationCompat.Builder mBuilder =
-                    new NotificationCompat.Builder(getApplicationContext())
-                            .setSmallIcon(getApplicationInfo().icon)
-                            .setContentTitle(title)
-                            .setContentText(message)
-                            .setSound(alarmSound)
-                            .setAutoCancel(true);
-            mBuilder.setContentIntent(pContentIntent);
-            onCreateLocalNotication(mBuilder);
+            showLocalNotication();
         }
     }
 
@@ -53,6 +40,24 @@ public class UdoIntentService extends GcmListenerService {
         NotificationManager mNotificationManager =
                 (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(81723469, mBuilder.build());
+    }
 
+    private void showLocalNotication() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(getApplicationInfo().icon)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setSound(alarmSound)
+                        .setAutoCancel(true);
+        mBuilder.setContentIntent(createLaunchPendingIntent());
+
+    }
+
+    private PendingIntent createLaunchPendingIntent() {
+        Intent launchIntent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(packageName)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK).putExtra(NotificationUDO, true);
+        PendingIntent pContentIntent = PendingIntent.getActivity(getApplicationContext(), 873459238, launchIntent, PendingIntent.FLAG_ONE_SHOT);
+        return pContentIntent;
     }
 }
