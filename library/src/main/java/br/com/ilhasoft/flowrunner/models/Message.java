16a886ed7bc971414c903aa000f0a6f5149f41a8
John Cordeiro <john.dalton@ilhasoft.com.br>
2016-06-30 18:18:08 -0300

    Add hashCode and equals message implementation

--- library/src/main/java/br/com/ilhasoft/flowrunner/models/Message.java
+++ library/src/main/java/br/com/ilhasoft/flowrunner/models/Message.java
@@ -178,4 +178,18 @@ public class Message {
         this.ruleset = ruleset;
         return this;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Message message = (Message) o;
+        return id != null ? id.equals(message.id) : message.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
